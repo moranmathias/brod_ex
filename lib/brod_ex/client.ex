eef410edb90c1eeb19a8cfb7d83e7c9adab5529c
Lukas Rieder <l.rieder@gmail.com>
2018-06-05 21:44:23 +0200

    add documentation to BrodEx.Client

--- lib/brod_ex/client.ex
+++ lib/brod_ex/client.ex
@@ -1,16 +1,37 @@
 defmodule BrodEx.Client do
-  @moduledoc false
+  @moduledoc """
+  Defines a client to interact with a Kafka cluster.
 
-  defmacro __using__(module_opts \\ []) do
-    quote bind_quoted: [module_opts: module_opts] do
-      @otp_app Keyword.fetch!(module_opts, :otp_app)
+  Similar to an Ecto Repo, you define your own module and configure connection
+  and client behaviour in your `config/config.exs.`
 
-      alias BrodEx.Config
+  When used the client expects the `:otp_app` as option. The `:otp_app` should point
+  to an OTP application that has the Kafka client configuration. For example, the client:
+
+      defmodule MyApp.Kafka do
+        use BrodEx.Client, otp_app: :my_app
+      end
+
+  Can be configured with:
+
+      config :your_app, YourApp.Kafka,
+        endpoints: "localhost:9092"
+
+  The client will transparently handle re-connection to your Kafka cluster.
+  """
+
+  @type t :: module
+
+  @doc false
+  defmacro __using__(use_opts \\ []) do
+    quote bind_quoted: [use_opts: use_opts] do
+      @behaviour BrodEx.Client
+      @otp_app Keyword.fetch!(use_opts, :otp_app)
 
       def start_link(opts \\ []) do
         client_config = Application.get_env(@otp_app, __MODULE__, [])
                         |> Keyword.merge(opts)
-                        |> Config.brod_client_config()
+                        |> BrodEx.Config.brod_client_config()
         clients = Application.get_env(:brod, :clients, [])
                   |> Keyword.merge([{__MODULE__, client_config}])
 
@@ -31,7 +52,24 @@ defmodule BrodEx.Client do
         BrodEx.produce_sync(__MODULE__, topic, partition, key, value)
       end
 
+      defoverridable child_spec: 1
     end
   end
 
+  @doc """
+  Starts a `:brod_client` and returns `{:ok, pid}`.
+
+  ## Options
+
+    * `:endpoints` – The `host:port` string(s) of the Kafka broker to connect to.
+      Can be given as a list of strings, or just a string that separates multiple `host:port`
+      entries with `;` or a ` ` (space).
+    * `:reconnect_cool_down_seconds` – the number of seconds to wait between re-connection attempts (default: 10)
+    * `:auto_start_producers` – set to false if you want to manually start your `:brod_producer` (default: true)
+    * `:default_producer_config` – set default options to be passed to the start of producers (default: [])
+  """
+  @callback start_link(opts :: Keyword.t) :: {:ok, pid} |
+                            {:error, {:already_started, pid}} |
+                            {:error, term}
 end
+
